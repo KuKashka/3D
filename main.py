e380343d48c1014d6d867c918072cf9d964e4221
KuKashka <ostovrah@gmail.com>
2025-01-23 20:32:16 +0200

    мда

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
-from models import *
 
 app = Ursina()
+
+from models import *
 
 sky=Sky(texture='sky_sunset')
 
 
 map = Map()
-map.new_map()
+map.new_map(size = 60)
 
 
-player = FirstPersonController(speed = 13, jump_height=3)
+player = Player(speed = 13, jump_height=3)
 
 
 app.run()
--- models.py
+++ models.py
@@ -2,12 +2,26 @@ from ursina import *
 from perlin_noise import PerlinNoise 
 from ursina.shaders import basic_lighting_shader
 from numpy import floor
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+
+import os
+
+block_textures = []
+BASE_DIR = os.getcwd()
+BLOCKS_DIR = os.path.join(BASE_DIR, 'assets/blocks/blocks')
+
+file_list = os.listdir(BASE_DIR)
+
+for image in file_list:
+     texture = load_texture('assets/blocks/blocks' + os.sep + image)
+     block_textures.append(texture)
 
 class Block(Entity):
     def __init__(self, block_type, pos,**kwargs):
             super().__init__(
             model='cube',
-            texture='grass',
+            texture=block_textures[block_type],
             position=pos,
             scale=1,
             collider='box',
@@ -26,9 +40,23 @@ class Map(Entity):
             self.blocks = {}
             self.noise = PerlinNoise(octaves = 3, seed=4522)
 
-    def new_map(self):
-        for x in range(20):
-            for z in range(20):
+    def new_map(self, size=30):
+        for x in range(50):
+            for z in range(50):
                 y = floor(self.noise([x/24, z/24])*6)
-                block = Block(0, (x,y,z))
+                block = Block(3, (x,y,z))
 
+class Player(FirstPersonController):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def input(self, key):
+        super().input(key)
+
+        if key == 'left mouse down' and mouse.hovered_entity:
+            destroy(mouse.hovered_entity)
+
+        if key == 'right mouse down' and mouse.hovered_entity:
+            hit_info = raycast(camera.world_position, camera.forward, distance=5)
+            if hit.info.hit:
+                Block(1, hit_info.entity.position + hit_info.normal)
